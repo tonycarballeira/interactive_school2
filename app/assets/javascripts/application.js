0de86199c2f0b931b6c742a872a0bf728bcf725e
Tony Carballeira <tonycarballeira@gmail.com>
2015-07-24 14:18:03 -0400

    modified js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require_tree 
 function cycleImages(){
 	      var $active = $('#image-carousel .active');
 	      var $next = ($active.next().length > 0) ? $active.next() : $('#image-carousel img:first');
