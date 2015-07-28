a82f651d5a864f7ba4518c978f1e0887b67b8084
Tony Carballeira <tonycarballeira@gmail.com>
2015-07-28 15:28:53 -0400

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,6 @@ ActiveRecord::Schema.define(version: 20150728181652) do
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -71,5 +70,4 @@ ActiveRecord::Schema.define(version: 20150728181652) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  add_foreign_key "subscriptions", "users"
 end
