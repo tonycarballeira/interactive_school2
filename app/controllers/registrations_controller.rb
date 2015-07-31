f54323ad348a7781db9d3a82357667191075a999
tonycarballeira <tonycarballeira@gmail.com>
2015-07-31 09:57:38 -0400

    modified view and controller for plan

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,15 +1,15 @@
 class RegistrationsController < Devise::RegistrationsController
 	def new
 		build_resource({})
-		self.resource.build_subscription
+		self.resource.build_plan
 		respond_with self.resource
 	end
 
 	def create
 		super
-		resource.subscription.ip_address = request.remote_ip
-		if !resource.subscription.errors.full_messages.any?
-			resource.subscription.save!
+		resource.plan.ip_address = request.remote_ip
+		if !resource.plan.errors.full_messages.any?
+			resource.plan.save!
 		end
 	end
 
@@ -17,15 +17,15 @@ class RegistrationsController < Devise::RegistrationsController
 
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.for(:sign_up) do |u|
-		  u.permit(:subscription_attributes => [:card_type, :card_expires_on])
+		  u.permit(:plan_attributes => [:card_type, :card_expires_on])
 		end
 		devise_parameter_sanitizer.for(:account_update) do |u|
-		  u.permit(:subscription_attributes => [:card_type, :card_expires_on])
+		  u.permit(:plan_attributes => [:card_type, :card_expires_on])
 		end
 	end
 	
 	def sign_up_params
-		params.require(resource_name).permit(:email, :password, :password_confirmation, :first_name, :last_name, :subscription_attributes => [:card_type, :card_expires_on, :first_name, :last_name, :card_number, :card_verification])
+		params.require(resource_name).permit(:email, :password, :password_confirmation, :first_name, :last_name, :plan_attributes => [:card_type, :card_expires_on, :first_name, :last_name, :card_number, :card_verification])
 	end
 
 end
