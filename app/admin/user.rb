2a3b2c9be50c000c9608ead32a708f9ada885a2c
tonycarballeira <tonycarballeira@gmail.com>
2015-08-09 20:25:54 -0400

    modified activeadmin user permitted params

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register User do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
- permit_params :email, :encrypted_password, :created_at, :password_confirmation, :sub_expire_date
+ permit_params :email, :first_name, :last_name, :sub_expire_date, :encrypted_password, :created_at, :password_confirmation, :sub_expire_date
 #
 # or
 #
