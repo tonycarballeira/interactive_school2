d9cd9c3fa865f044949f44e85141e73992b29e5c
tonycarballeira <tonycarballeira@gmail.com>
2015-08-22 13:14:36 -0400

    schema commit

--- db/schema.rb
+++ db/schema.rb
@@ -49,8 +49,6 @@ ActiveRecord::Schema.define(version: 20150822155853) do
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 
-  add_index "plans", ["user_id"], name: "index_plans_on_user_id", using: :btree
-
   create_table "subscriptions", force: :cascade do |t|
     t.string   "new"
     t.string   "ip_address"
@@ -95,6 +93,5 @@ ActiveRecord::Schema.define(version: 20150822155853) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  add_foreign_key "plans", "users"
   add_foreign_key "subscriptions", "users"
 end
