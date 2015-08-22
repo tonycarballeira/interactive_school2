ad413741afb4c8fe29ddab58479bd86dafa57548
Tony Carballeira <tonycarballeira@Tonys-MacBook-Pro-2.local>
2015-08-22 12:41:52 -0400

    setup for migration of old users

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -199,7 +199,7 @@ Devise.setup do |config|
   # REST_AUTH_SITE_KEY to pepper).
   #
   # Require the `devise-encryptable` gem when using anything other than bcrypt
-  # config.encryptor = :sha512
+  config.encryptor = :sha
 
   # ==> Scopes configuration
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
